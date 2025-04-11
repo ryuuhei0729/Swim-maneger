ca4783265a8ea4ac3c1ee40ce8db9bb67912a519
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-12 01:29:12 +0900

    デザイン進化！

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user_auth!
+
+  def current_user
+    current_user_auth&.user
+  end
+  helper_method :current_user
 end
