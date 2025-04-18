4d5852f9c0bb59868a0cda25c882a6851a9730a8
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-18 21:13:36 +0900

    不要なファイルとディレクトリを削除
    - UsersControllerとそのテストファイルを削除
      - ユーザー管理機能はAdminControllerに統合されているため重複を解消
      - ルーティングが設定されていない未使用のコントローラーを削除
    - 空のビューディレクトリ(app/views/users/)を削除
      - 削除したコントローラーに関連する未使用のビューディレクトリを整理
    
    コードベースの整理とメンテナンス性の向上

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,32 @@
 Rails.application.routes.draw do
-  get "admin/index"
-  devise_for :user_auths, controllers: {
-    sessions: 'user_auths/sessions'
-  }
+  # ルートパス
   devise_scope :user_auth do
     root to: 'user_auths/sessions#new'
   end
-  
+
+  # 認証関連
+  devise_for :user_auths, controllers: {
+    sessions: 'user_auths/sessions'
+  }
+
+  # 全員見れるページ
   get 'home', to: 'home#index', as: :home
   get 'mypage', to: 'mypage#index', as: :mypage
   patch 'mypage', to: 'mypage#update'
-  
   get 'member', to: 'member#index', as: :member
+
+  # 管理者のみのページ
   get 'admin', to: 'admin#index', as: :admin
+  get 'admin/index'
+  
+  # [管理者]新規登録
   get 'admin/create_user', to: 'admin#create_user', as: 'admin_create_user'
   post 'admin/users', to: 'admin#create_user', as: 'admin_users'
   patch 'admin/users', to: 'admin#create_user'
   
-  # お知らせ管理機能のルーティング
+  # [管理者]お知らせ管理
+  delete 'admin/announcement/:id', to: 'admin#destroy_announcement', as: 'admin_destroy_announcement'
+  patch 'admin/announcement/:id', to: 'admin#update_announcement', as: 'admin_update_announcement'
   get 'admin/announcement', to: 'admin#announcement', as: 'admin_announcement'
   post 'admin/announcement', to: 'admin#create_announcement'
-  patch 'admin/announcement/:id', to: 'admin#update_announcement', as: 'admin_update_announcement'
-  delete 'admin/announcement/:id', to: 'admin#destroy_announcement', as: 'admin_destroy_announcement'
 end
