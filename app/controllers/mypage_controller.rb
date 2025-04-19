3aa44495ff81e6a6c78b7bdb5de18ed8bd0a0fb7
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-19 15:23:34 +0900

    プロフィール画像アップロード時のクライアントサイドバリデーションを追加
    - ファイル選択フィールドにaccept属性を追加し、JPGとPNGのみを許可
    - JavaScriptによるファイル形式のバリデーション機能を実装
    - 無効なファイル形式が選択された場合、エラーメッセージを表示し送信ボタンを無効化
    - モーダル内でエラーメッセージを表示するように改善

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -8,16 +8,26 @@ class MypageController < ApplicationController
 
   def update
     @user = current_user_auth.user
+    
+    # 画像形式のチェック
+    if params[:user][:avatar].present?
+      content_type = params[:user][:avatar].content_type
+      unless ['image/jpeg', 'image/png'].include?(content_type)
+        redirect_to mypage_path, alert: 'JPGまたはPNG形式の画像のみアップロード可能です'
+        return
+      end
+    end
+    
     if @user.update(user_params)
-      redirect_to mypage_path, notice: '自己紹介を更新しました'
+      redirect_to mypage_path, notice: 'プロフィールを更新しました'
     else
-      redirect_to mypage_path, alert: '自己紹介の更新に失敗しました'
+      redirect_to mypage_path, alert: 'プロフィールの更新に失敗しました'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:bio)
+    params.require(:user).permit(:bio, :avatar)
   end
 end
