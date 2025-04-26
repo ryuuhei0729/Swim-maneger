349638a6be08c03c2914826e5903a438bbcb2f97
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-26 17:21:36 +0900

    tailwindcss-modal を導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,8 @@
 import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import "./controllers"
+import { Modal, Ripple, initTE } from "tw-elements";
 
 // Stimulusアプリケーションの設定
 const application = Application.start()
@@ -9,6 +11,8 @@ application.debug = true
 
 // コントローラーを自動読み込み
 eagerLoadControllersFrom("controllers", application)
+
+initTE({ Modal, Ripple });
 
 export { application }
 
---
+++ config/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require('tw-elements/dist/plugin.cjs')
+  ]
+} 
