2757713d6bf55413f33832ea29751c360fa229b3
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-27 19:57:42 +0900

    importmap周りの設定を変更

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,20 +1,12 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "tw-elements"
 import "@hotwired/turbo-rails"
 import { Application } from "@hotwired/stimulus"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-import "./controllers"
-import { Modal, Ripple, initTE } from "tw-elements";
 
-// Stimulusアプリケーションの設定
 const application = Application.start()
 application.debug = true
 
 // コントローラーを自動読み込み
 eagerLoadControllersFrom("controllers", application)
-
-initTE({ Modal, Ripple });
-
 export { application }
-
-
 
