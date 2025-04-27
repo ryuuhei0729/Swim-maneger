21173185e7befc5873ca8b17b426d0cca7a1322e
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-27 21:28:21 +0900

    モーダルのサンプルをホーム画面に導入

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "tw-elements", to: "https://cdn.jsdelivr.net/npm/tw-elements@1.0.0-beta2/dist/js/tw-elements.umd.min.js"
 # コントローラーを明示的に追加
 pin "controllers/calendar_controller", to: "controllers/calendar_controller.js"
+pin "controllers/modal_controller", to: "controllers/modal_controller.js"
 # 他のコントローラーは自動 pin
 pin_all_from "app/javascript/controllers", under: "controllers/"
