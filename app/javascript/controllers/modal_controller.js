7e7da64cc53bb6080350ee302e9c3f050e9c120d
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-28 02:24:30 +0900

    member/index.html.erbのモーダルを改善

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -4,35 +4,21 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["modal"];
 
-  connect() {
-    console.log("Modal controller connected");
+  //一旦「モーダル外クリック閉じる」は諦めるか
+  // connect() {
+  //   this.modalTarget.classList.add("hidden");
+  //   this.modalTarget.addEventListener("click", (e) => {
+  //     if (e.target === this.modalTarget) {
+  //       this.close();
+  //     }
+  //   });
+  // }
+
+  open() {
+    this.modalTarget.classList.remove("hidden");
+  }
+
+  close() {
     this.modalTarget.classList.add("hidden");
-    this.modalTarget.addEventListener("click", (e) => {
-      if (e.target === this.modalTarget) {
-        this.close();
-      }
-    });
-    document.addEventListener('keydown', this.handleKeydown.bind(this));
-  }
-
-  disconnect() {
-    document.removeEventListener('keydown', this.handleKeydown.bind(this));
-  }
-
-  open(event) {
-    const modalId = event.currentTarget.dataset.modalTarget;
-    const modal = document.getElementById(modalId);
-    if (modal) {
-      modal.classList.remove("hidden");
-      document.body.style.overflow = "hidden";
-    }
-  }
-
-  close(event) {
-    const modal = event.currentTarget.closest('[data-modal-target="modal"]');
-    if (modal) {
-      modal.classList.add('hidden');
-      document.body.style.overflow = '';
-    }
   }
 }
