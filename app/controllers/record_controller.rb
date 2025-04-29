9d79fe20f1a81e886de78f67adbc3856c66c726c
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-29 15:58:49 +0900

    並び替え機能を実装しました。

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,12 +1,11 @@
 class RecordController < ApplicationController
   def index
-    @players = User.where(user_type: 'player').order(generation: :desc)
-    @players_by_generation = @players.group_by(&:generation)
+    @players = User.where(user_type: 'player').order(generation: :asc)
     @male_players = @players.select { |p| p.gender == 'male' }
     @female_players = @players.select { |p| p.gender == 'female' }
-    @male_players_by_generation = @male_players.group_by(&:generation)
-    @female_players_by_generation = @female_players.group_by(&:generation)
-    @default_tab = current_user_auth.user.gender == 'male' ? 'male' : 'female'
+    @default_tab = params[:tab] || (current_user_auth.user.gender == 'male' ? 'male' : 'female')
+    @sort_by = params[:sort_by]
+    @sort_direction = params[:sort_direction] || 'asc'
     @events = [
       { id: '50m_fr', title: '50m自由形', style: '自由形' },
       { id: '100m_fr', title: '100m自由形', style: '自由形' },
@@ -27,5 +26,41 @@ class RecordController < ApplicationController
       { id: '400m_im', title: '400m個人メドレー', style: '個人メドレー' }
     ]
     @best_times = BestTimeTable.includes(:user)
+
+    # 並び替え処理
+    if @sort_by.present?
+      @players = sort_players_by_time(@players, @sort_by, @sort_direction)
+      @male_players = sort_players_by_time(@male_players, @sort_by, @sort_direction)
+      @female_players = sort_players_by_time(@female_players, @sort_by, @sort_direction)
+    else
+      # デフォルト表示用のグループ化
+      @players_by_generation = @players.group_by(&:generation)
+      @male_players_by_generation = @male_players.group_by(&:generation)
+      @female_players_by_generation = @female_players.group_by(&:generation)
+    end
+  end
+
+  private
+
+  def sort_players_by_time(players, sort_by, direction)
+    players.sort_by do |player|
+      best_time = @best_times.find { |bt| bt.user_id == player.id }
+      time = best_time&.send(sort_by)
+      
+      # タイムを秒数に変換
+      if time.present? && time != '-'
+        minutes, seconds = time.split(':')
+        if seconds.present?
+          total_seconds = minutes.to_i * 60 + seconds.to_f
+        else
+          total_seconds = minutes.to_f
+        end
+      else
+        # タイムが無い場合は無限大として扱う
+        total_seconds = Float::INFINITY
+      end
+      
+      direction == 'asc' ? total_seconds : -total_seconds
+    end
   end
 end 
