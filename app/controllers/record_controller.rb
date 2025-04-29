f93d8bf949b367ba0ca8e5cc267229afae1ad8e6
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-04-29 15:03:37 +0900

    ログイン者の性別によってデフォルト表示を切り替え。

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -2,6 +2,11 @@ class RecordController < ApplicationController
   def index
     @players = User.where(user_type: 'player').order(generation: :desc)
     @players_by_generation = @players.group_by(&:generation)
+    @male_players = @players.select { |p| p.gender == 'male' }
+    @female_players = @players.select { |p| p.gender == 'female' }
+    @male_players_by_generation = @male_players.group_by(&:generation)
+    @female_players_by_generation = @female_players.group_by(&:generation)
+    @default_tab = current_user_auth.user.gender == 'male' ? 'male' : 'female'
     @events = [
       { id: '50m_fr', title: '50m自由形', style: '自由形' },
       { id: '100m_fr', title: '100m自由形', style: '自由形' },
