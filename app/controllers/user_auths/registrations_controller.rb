c89498dfa5bc6b638b07a5aaef2dbde3b798f28f
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-06-05 23:27:07 +0900

    メインどころのレビュー

--- app/controllers/user_auths/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-class UserAuths::RegistrationsController < Devise::RegistrationsController
-  protected
-
-  def after_sign_up_path_for(resource)
-    session[:user_auth_id] = resource.id
-    new_user_path
-  end
-end 
--- app/models/milestone.rb
+++
@@ -1,8 +0,0 @@
-class Milestone < ApplicationRecord
-  belongs_to :objective
-  has_one :milestone_review, dependent: :destroy
-
-  validates :milestone_type, presence: true, inclusion: { in: ['quality', 'quantity'] }
-  validates :limit_date, presence: true
-  validates :note, presence: true
-end 
