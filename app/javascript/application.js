e54850d8543175d41e64b2e15df8c63ec0abd5f8
ryuhei.hosoi <ryuuhei_0729@yahoo.co.jp>
2025-08-07 23:21:02 +0900

    サイドバーのハンバーガーメニューを実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,10 +6,7 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 const application = Application.start()
 application.debug = true
 
-// デバッグ用
-console.log("Stimulus application started")
-console.log("Registered controllers:", application.controllers)
-
 // コントローラーを自動読み込み
 eagerLoadControllersFrom("controllers", application)
+
 export { application }
